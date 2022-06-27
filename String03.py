0836641df964ea978a8e5c41ebc80587ba848b3e
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-06-27 16:36:29 +0500

    String03

--- String03.py
+++ String03.py
@@ -1,3 +1,6 @@
+from re import X
+
+
 def main():
     """
     "*" Return a string that consists of only this character and is five in length.
@@ -6,4 +9,6 @@ def main():
     Returns:
         str: return answer.
     """
-    return 
+    x = "code "
+    return x
+print(main())
